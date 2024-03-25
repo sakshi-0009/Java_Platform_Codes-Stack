40c84d8b98d0871e4ba569dffb795b2434fda41d
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-25 22:30:33 +0530

    Remove Outermost Parentheses

---
+++ 03_Remove Outermost Parentheses.java
@@ -0,0 +1,34 @@
+/* Problem Statement :-
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+*/
+
+// Answer :-
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int openCount = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (openCount > 0) {
+                    result.append(c);
+                }
+                openCount++;
+            } else if (c == ')') {
+                openCount--;
+                if (openCount > 0) {
+                    result.append(c);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
