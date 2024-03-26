beedbf15c7b5735b325d322512ac7e29e0348195
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-26 22:35:57 +0530

    Delete middle element of a stack

---
+++ 06_Delete middle element of a stack.java
@@ -0,0 +1,25 @@
+/* Problem Statement :-
+Given a stack, delete the middle element of the stack without using any additional data structure.
+Middle element:- floor((size_of_stack+1)/2) (1-based indexing) from bottom of the stack.
+*/
+
+// Answer :-
+
+class Solution {
+    public void deleteMid(Stack<Integer> s, int sizeOfStack) {
+        deleteMidUtil(s, (sizeOfStack + 1) / 2, 1);
+    }
+
+    private void deleteMidUtil(Stack<Integer> s, int mid, int current) {
+        if (s.isEmpty()) {
+            return;
+        }
+        
+        int x = s.pop();
+        
+        if (current != mid) {
+            deleteMidUtil(s, mid, current + 1);
+            s.push(x);
+        }
+    }
+}
