2fa67c7af9c5031dd3f67da9342da7a2ebabc53c
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-31 19:24:05 +0530

    Online Stock Span

--- 09_Online Stock Span.java
+++ 09_Online Stock Span.java
@@ -13,3 +13,22 @@ int next(int price) Returns the span of the stock's price given that today's pri
 
 // Answer :-
 
+class StockSpanner {
+    Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+    
+        while (!stack.isEmpty() && price >= stack.peek()[0]) {
+            span += stack.pop()[1];
+        }
+        
+        stack.push(new int[]{price, span});
+        
+        return span;
+    }
+}
