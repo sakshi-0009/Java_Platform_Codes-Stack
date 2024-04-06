6916cec47792a4cefa0cb14805531c1003f872f7
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-04-06 22:25:53 +0530

    Implement Stack using Queues

---
+++ 17_Implement Stack using Queues.java
@@ -0,0 +1,61 @@
+/* Problem Statement :-
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+*/
+
+// Answer :-
+
+class MyStack {
+
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+    
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue1.offer(x);
+    }
+    
+    public int pop() {
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        
+        int popped = queue1.poll();
+        
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        
+        return popped;
+    }
+    
+    public int top() {
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        
+        int topElement = queue1.peek();
+        
+        queue2.offer(queue1.poll());
+        
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        
+        return topElement;
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
