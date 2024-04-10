d79f18a16a1a952b88f0a12e2547d7539e2b269d
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-04-10 19:43:09 +0530

    Implement Queue using array

---
+++ 23_Implement Queue using array.java
@@ -0,0 +1,39 @@
+/* Problem Statement :-
+Implement a Queue using an Array. Queries in the Queue are of the following type:
+(i) 1 x   (a query of this type means  pushing 'x' into the queue)
+(ii) 2     (a query of this type means to pop element from queue and print the poped element)
+*/
+
+// Answer :-
+
+class MyQueue {
+
+    private int front, rear;
+    private int[] arr;
+    private int maxSize;
+
+    public MyQueue() {
+        this.front = 0;
+        this.rear = 0;
+        this.maxSize = 100005; // Maximum size of the queue
+        this.arr = new int[maxSize];
+    }
+
+    public void push(int x) {
+        if (rear == maxSize) {
+            System.out.println("Queue is full. Cannot push element.");
+            return;
+        }
+        
+        arr[rear++] = x;
+    }
+
+    public int pop() {
+        if (front == rear) {
+            return -1; // Return -1 if queue is empty (considering it's an integer queue)
+        }
+
+        int poppedElement = arr[front++];
+        return poppedElement;
+    }
+}
